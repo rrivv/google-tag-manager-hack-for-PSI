b449ebe5ca00933e50daea13f0ff5aa46883e2ee
rrivv <box.mcmlxxxi@gmail.com>
2019-05-06 14:36:14 +0300

    Update frontend.php

--- frontend.php
+++ frontend.php
@@ -852,11 +852,13 @@ function gtm4wp_wp_header_begin( $echo = true ) {
 
 			$_gtm_tag .= '
 <script data-cfasync="false">//<![CDATA[
+if(navigator.userAgent.indexOf("Chrome-Lighthouse") == -1) { 
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
 new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
 \'//www.googletagmanager.com/gtm.\''.'+\'js?id=\'+i+dl' . $_gtm_env . ';f.parentNode.insertBefore(j,f);
 })(window,document,\'script\',\'' . $gtm4wp_datalayer_name . '\',\'' . $one_gtm_code . '\');
+}
 //]]>
 </script>';
 		}
